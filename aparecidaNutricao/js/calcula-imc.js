88a38fc716f6c73bcc2ac5c713780ce6ed7cb470
erikasimona <erikasimona.design@gmail.com>
2020-10-09 00:06:42 -0300

    Adicionar e remover pacientes

--- aparecidaNutricao/js/calcula-imc.js
+++ aparecidaNutricao/js/calcula-imc.js
@@ -1,13 +1,8 @@
-//	alert("Olá Mundo");
-//console.log("Oi, Mundo");
-//console.log(document);
-
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
 for(var i = 0; i < pacientes.length ; i++){
+
     var paciente = pacientes[i];
    
     var tdPeso = paciente.querySelector(".info-peso");
@@ -17,30 +12,47 @@ for(var i = 0; i < pacientes.length ; i++){
     var peso = tdPeso.textContent;
     var altura = tdAltura.textContent;
 
-    var alturaEhValida = true;
-    var pesoEhValido = true;
+    var alturaEhValida = validaAltura(altura);
+    var pesoEhValido = validaPeso(peso);
 
-    if(peso <= 0 || peso > 1000){
+    if(!pesoEhValido){
         console.log("Peso inválido");
         tdImc.textContent = "Peso inválido!";
         pesoEhValido = false;
-        
         paciente.classList.add("paciente-invalido");
     }
     
-    if(altura <= 0 || altura >= 3){
+    if(!alturaEhValida){
         console.log("Altura inválida");
         tdImc.textContent = "Altura inválida!";
         alturaEhValida = false;
-
         paciente.classList.add("paciente-invalido");
     }
-    
+
+
     if(pesoEhValido && alturaEhValida){
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
     } else {
-        tdImc.textContent = "Altura e/ou peso inválidos!"
+      tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+}
+
+
+
+function validaPeso(peso){
+    if (peso >= 0 && peso < 1000){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 3.00){
+        return true;
+    } else { 
+        return false;
     }
 }
 
@@ -48,8 +60,4 @@ function calculaImc(peso, altura){
     var imc = 0;
     imc = peso / (altura * altura);
     return imc.toFixed(2);
-}
-
-function validaPeso(){
-    
 }
