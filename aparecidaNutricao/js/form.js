2da9ef244d4b7b022b50fe88b3a86f9bbdcbe098
erikasimona <erikasimona.design@gmail.com>
2020-10-11 16:29:31 -0300

    finalização
    filtros,formulario

--- aparecidaNutricao/js/form.js
+++ aparecidaNutricao/js/form.js
@@ -7,21 +7,24 @@ botaoAdicionar.addEventListener("click", function(event){
    var paciente = obtemPacienteDoFormulario(form);
    
    var erros = validaPaciente(paciente);
-   
+   console.log(erros);
    if (erros.length > 0) {
        exibeMensagensDeErro(erros);
        return;
    }
 
-   var pacienteTr = montaTr(paciente);
-
-   var tabela = document.querySelector("#tabela-pacientes");
-   tabela.appendChild(pacienteTr);
+   adicionaPacienteNaTabela(paciente)
    form.reset();
 
    var mensagensErro = document.querySelector("#mensagens-erro");
    mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteNaTabela(paciente){
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obtemPacienteDoFormulario(form){
     var paciente={
