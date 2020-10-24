c5d1f1c21159d398dc3c6ed847371d9fcfe36726
erikasimona <erikasimona.design@gmail.com>
2020-10-24 13:32:52 -0300

    Jquery modulo 2

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -6,9 +6,14 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
-    $("#botao-reiniciar").click(reiniciaJogo);
     inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
+
+function atualizaTempoInicial(tempo){
+    tempoInicial = tempo;
+    $("#tempo-digitacao").text(tempo);
+}
 
 function atualizaTamanhoFrase(){
     var frase = $(".frase").text();
@@ -30,8 +35,8 @@ function inicializaContadores(){
 }
 
 function inicializaCronometro(){
-    var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function(){
+        var tempoRestante = $("#tempo-digitacao").text();
         $("#botao-reiniciar").attr("disabled", true);
         var cronometroID = setInterval(function(){
             tempoRestante--;
@@ -54,8 +59,8 @@ function finalizaJogo(){
 }
 
 function inicializaMarcadores(){
-    var frase = $(".frase").text();
     campo.on("input", function(){
+        var frase = $(".frase").text();
         var digitado = campo.val();
         var comparavel = frase.substr(0, digitado.length);
         if(digitado == comparavel){
